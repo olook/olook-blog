642aabfca6d64b97d9e93a36c1cf4219362bfb22
Rafael Carvalho <rafa1402@gmail.com>
2013-03-21 18:54:30 -0300

    customize olook tv page

--- wp-content/themes/wt_metro/page-full.php
+++ wp-content/themes/wt_metro/page-full.php
@@ -13,11 +13,27 @@
 
 <section id="primary">
 	<div id="content" class="full-content" role="main">
-	<?php if (have_posts()) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>				
-			<?php get_template_part( 'content', 'page' ); ?>
-		<?php endwhile; // end of the loop. ?>
-	<?php endif ?>
+   	<?php if (have_posts()) : ?>
+   		<?php while ( have_posts() ) : the_post(); ?>				
+   			<?php get_template_part( 'content', 'page' ); ?>
+   		<?php endwhile; // end of the loop. ?>
+   	<?php endif ?>
+   
+   	<div id="featured-cats">
+      	<?php
+      		//include featured categories
+      		if ( wt_get_option( 'wt_feat_cat1' ) != 0 ) {
+      			get_template_part( 'includes/feat-cat1' );						
+      		}
+
+      	?>
+   
+         <div class="mais-vistos">
+            <h3>Posts mais vistos</h3>
+            <?php the_widget('wellthemes_popular_posts_widget', 'number=5'); ?>	
+         </div>
+   	</div>
+   
 	</div><!-- /content -->
 </section><!-- /primary -->
 

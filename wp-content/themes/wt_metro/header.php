e88021e61d5c74d45a3d11afb313ec1da81b6ad6
Rafael Carvalho <rafa1402@gmail.com>
2013-05-10 14:58:38 -0300

    changed UA to olook.com.br

--- wp-content/themes/wt_metro/header.php
+++ wp-content/themes/wt_metro/header.php
@@ -80,7 +80,7 @@
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-26610963-5']);
+  _gaq.push(['_setAccount', 'UA-26610963-1']);
   _gaq.push(['_setDomainName', 'olook.com.br']);
   _gaq.push(['_trackPageview']);
 

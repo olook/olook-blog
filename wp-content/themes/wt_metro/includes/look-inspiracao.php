4387d673b0e096685cee3cb1b882a9101bae6000
Rafael Carvalho <rafa1402@gmail.com>
2013-06-21 18:34:23 -0300

    adjust layout

--- wp-content/themes/wt_metro/includes/look-inspiracao.php
+++ wp-content/themes/wt_metro/includes/look-inspiracao.php
@@ -48,8 +48,8 @@
                <?php 
                  //display only first 150 characters in the excerpt.               
                  $excerpt = get_the_excerpt();                               
-                 echo mb_substr($excerpt,0, 55);   
-                 if (strlen($excerpt) > 54){ 
+                 echo mb_substr($excerpt,0, 65);   
+                 if (strlen($excerpt) > 64){ 
                    echo '...'; 
                  } 
                ?>

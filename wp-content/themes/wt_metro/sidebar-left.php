9a3693b846457b9082deebe85b2d7ee78e2573f0
Thaiane Gazzi <thaianegazzi@gmail.com>
2013-06-24 10:46:19 -0300

    IMG

--- wp-content/themes/wt_metro/sidebar-left.php
+++ wp-content/themes/wt_metro/sidebar-left.php
@@ -36,7 +36,7 @@
 		<?php endif; // end sidebar widget area ?>
 
 		<div class="banner-post">
-			<a href="http://www.olook.com.br/colecoes/animal-print"><img src="wp-content/themes/wt_metro/images/gifs/banner_animal_print.gif" alt="" /></a>
+			<a href="http://www.olook.com.br/colecoes/animal-print"><img src="http://www.olook.com.br/stylist-news/wp-content/themes/wt_metro/images/gifs/banner_animal_print.gif" alt="" /></a>
 		</div>
 		
 </div><!-- /sidebar -->

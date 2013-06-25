f35c0a38a2ca6e3a00b15a46b606a86e8d5a532b
Rafael Carvalho <rafa1402@gmail.com>
2013-06-25 14:00:25 -0300

    adjust layout

--- wp-content/themes/wt_metro/js/functions.js
+++ wp-content/themes/wt_metro/js/functions.js
@@ -2,14 +2,13 @@ function menuLine(){
   var list = $("#menu-principal li ul.sub-menu");
   if(list.length > 0){
     list.each(function(){
-     $(this).mouseover(function(){
-       $(this).prev().css({'border-left': 'solid 1px #000','border-right': 'solid 1px #000'});  
-     }).mouseout(function(){
-       $(this).prev().css({'border-left': 'solid 1px #FFF','border-right': 'solid 1px #FFF'});
-     })
+       $(this).mouseover(function(){
+         $(this).prev().css({'border-left': 'solid 1px #000','border-right': 'solid 1px #000'});  
+       }).mouseout(function(){
+         $(this).prev().css({'border-left': 'solid 1px #FFF','border-right': 'solid 1px #FFF'});
+       })
     })
- }
-  
+  }
 }
 
 function slideMenuBar(){

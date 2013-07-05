3f4e7167fbd1d65b4fba4f4be65c9806ff534b48
Thaiane Gazzi <thaianegazzi@gmail.com>
2013-07-05 17:17:27 -0300

    modified banner landing

--- wp-content/themes/wt_metro/includes/video.php
+++ wp-content/themes/wt_metro/includes/video.php
@@ -5,5 +5,5 @@
 
 <div class="banner-destaque">
 	<a href="http://www.olook.com.br/roupas?utf8=%E2%9C%93&id=1&category_id=4&sort_filter=&cloth_subcategories[]=top-cropped">
-		<img src="wp-content/themes/wt_metro/images/gifs/top_cropped.gif" alt="" /></a>
+		<img src="wp-content/themes/wt_metro/images/stylist_news_roupas.jpg" alt="" /></a>
 </div>

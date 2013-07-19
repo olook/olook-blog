989b860b674a109c69e935f10a487c77f2c5c28e
Rafael Carvalho <rafa1402@gmail.com>
2013-07-19 15:03:33 -0300

    adjust of GA

--- wp-content/themes/wt_metro/header.php
+++ wp-content/themes/wt_metro/header.php
@@ -76,23 +76,6 @@
 </head>
 <body <?php body_class(); ?>>
 
-<!-- Google Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-26610963-1']);
-  _gaq.push(['_setDomainName', 'olook.com.br']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-
    <div id="container" class="hfeed">	
 	<div id="content-blog">	
       <header id="new_header">

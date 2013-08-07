0da14faab5ab80726990918baff9d8ea30ca986a
Thaiane Gazzi <thaianegazzi@gmail.com>
2013-08-07 13:55:01 -0300

    modified banner

--- wp-content/themes/wt_metro/sidebar-left.php
+++ wp-content/themes/wt_metro/sidebar-left.php
@@ -36,7 +36,7 @@
 		<?php endif; // end sidebar widget area ?>
 
 		<div class="banner-post">
-			<a class="side-banner-post" href="http://www.olook.com.br/colecoes/animal-print/?utf8=%E2%9C%93&por=menor-preco"><img src="http://www.olook.com.br/stylist-news/wp-content/themes/wt_metro/images/gifs/banner_animal_print2.gif" alt="" /></a>
+			<a class="side-banner-post" href="http://www.olook.com.br/roupa/redley-carina%20duek-tan%20tan?utf8=?&por=menor-preco"><img src="http://www.olook.com.br/stylist-news/wp-content/themes/wt_metro/images/gifs/redley_CD_tantan_stylistnews.gif" alt="" /></a>
 		</div>
 		
 </div><!-- /sidebar -->

d86d1ed70f262555c2e69f7b125b561493bc4e75
Dommit <dommit@gmail.com>
2013-11-29 11:01:19 -0200

    bug fixed

--- wp-content/themes/wt_metro/header.php
+++ wp-content/themes/wt_metro/header.php
@@ -52,7 +52,7 @@
 </title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="http://localhost/wp-content/themes/wt_metro/teste.css" />
+<link rel="stylesheet" type="text/css" media="all" href="http://stylist-news.olook.com.br/wp-content/themes/wt_metro/teste.css" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
